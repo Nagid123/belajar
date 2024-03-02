922ea810ac34400ade6a28debee482ec11ec9ef2
Nagid123 <nagidd9@gmail.com>
2024-03-02 12:13:31 +0700

    belajar lagi 2

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<tr>
+                <td>NIS</td>
+                <td>Nama</td>
+                <td>Kelas</td>
+            </tr>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +12,25 @@
 </head>
 
 <body>
-    <h1>tips and trick </h1>
-    <h2>gatau</h2>
+    <div id="tabel-data-siswa">
+        <h1>Tabel Data Siswa</h1>
+        <table>
+            <tr>
+                <td>NIS</td>
+                <td>Nama</td>
+                <td>Kelas</td>
+            </tr>
+            <tr>
+                <td>128936123</td>
+                <td>Jaya</td>
+                <td>XII RPL 1</td>
+            </tr>
+            <tr>
+                <td>7ASDASD</td>
+                <td>Abdul</td>
+                <td>XII RPL 1</td>
+            </tr>
+        </table>
+    </div>
 </body>
-
 </html>
